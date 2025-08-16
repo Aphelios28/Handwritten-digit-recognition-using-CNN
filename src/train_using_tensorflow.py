fbbee9c44bf677f22d4992902189ffb645ea5472
Aphelios28 <truongphamnhat2004@gmail.com>
2025-08-16 16:23:04 +0700

    update file train.py

---
+++ src/train_using_tensorflow.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+
+
+
+def build_CNN():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Reshape((28, 28, 1), input_shape=(28, 28)))  # Thêm kênh màu (grayscale -> 1 channel)
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(64, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.Dense(10, activation='softmax'))
+    return model
+
+
+
+if __name__ == '__main__':
+    mnist = tf.keras.datasets.mnist
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = tf.keras.utils.normalize(x_train, axis=1)
+    x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+    model = build_CNN()
+
+    model.compile(optimizer='adam', loss ='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs = 10)
+
+    model.save('digit_recog.model')
--- train.py
+++ train.py
@@ -1,29 +1,50 @@
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
-import tensorflow as tf
+import torch
+from torch import nn
+from timeit import default_timer as timer
+from torch.utils.data import DataLoader
+from src.datasets import Fashion
+from src.train_using_pytorch import CNN
+from helper_functions import accuracy_fn, print_train_time
 
-mnist = tf.keras.datasets.mnist
+def train (train_data_loader, test_data_loader):
+    torch.manual_seed(42)
+    time_train_start_model = timer()
+    epochs = 3
+    model = CNN(input_shape=784, hidden_units=10, output_shape=len(Fashion.class_name()))
+    model.to('cpu')
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
+    for epoch in range(epochs):
+        print (f"Epoch: {epoch}")
+        train_loss = 0
+        for batch, (X, y) in enumerate(train_data_loader):
+            y_pred = model(X)
+            loss = loss_fn(y_pred, y)
+            train_loss += loss
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if batch % 400 == 0:
+                print (f"Looked at {batch * len(X)}/{len(train_data_loader.dataset)} batch")
+        train_loss /= len(train_data_loader)
+        ##test
+        test_loss, test_acc = 0, 0
+        with torch.inference_mode():
+            for X, y in test_data_loader:
+                test_pred = model(X)
+                test_loss += loss_fn(test_pred, y)
+                test_acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+            test_loss /= len(test_data_loader)
+            test_acc /= len(test_data_loader)
+        print (f"\nTrain loss :{train_loss:.5f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%\n")
+    time_train_end_model = timer()
+    print_train_time(start= time_train_start_model,end= time_train_end_model, device=str(next(model.parameters()).device))
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-x_train = tf.keras.utils.normalize(x_train, axis = 1)
-x_test = tf.keras.utils.normalize(x_test, axis = 1)
-
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Reshape((28, 28, 1), input_shape=(28, 28)))  # Thêm kênh màu (grayscale -> 1 channel)
-model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(64, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
+if __name__ == '__main__':
+    BATCH_SIZE = 32
+    train_data_loader = DataLoader(Fashion.load_data_train(), batch_size=BATCH_SIZE, shuffle=True)
+    test_data_loader = DataLoader(Fashion.load_data_test(), batch_size=BATCH_SIZE, shuffle=False)
+    train(train_data_loader, test_data_loader)
 
 
-model.compile(optimizer='adam', loss ='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs = 5)
-
-model.save('digit_recog.model')
