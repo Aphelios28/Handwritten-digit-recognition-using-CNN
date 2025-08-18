3580bd364d9e0abae82d1c2316348a9233d4fd3a
Aphelios28 <truongphamnhat2004@gmail.com>
2025-08-18 17:58:28 +0700

    change the dataset fashion into dataset number

--- src/datasets.py
+++ src/datasets.py
@@ -4,19 +4,20 @@ from torch import nn
 from torchvision import datasets, transforms
 from torch.utils.data import Dataset
 
-class Fashion(Dataset):
+class NumberMNIST(Dataset):
     def __init__(self, root = 'data', download = True, transform = transforms.ToTensor(), target_transform = None):
         self.root = root
         self.transform = transform
         self.target_transform = target_transform
         self.download = download
     def load_data_train(self):
-        train_data = datasets.FashionMNIST(root=self.root, train = True, download= self.download, transform=self.transform,
+        train_data = datasets.MNIST(root=self.root, train = True, download= self.download, transform=self.transform,
                                            target_transform=self.target_transform)
         return train_data
     def load_data_test(self):
-        test_data = datasets.FashionMNIST(root=self.root, train = False, download= self.download, transform=self.transform,
+        test_data = datasets.MNIST(root=self.root, train = False, download= self.download, transform=self.transform,
                                            target_transform=self.target_transform)
         return test_data
+    @staticmethod
     def class_name(self):
         return self.load_data_train().classes
